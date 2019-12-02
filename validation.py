e9be5d8769c7070c894ec168c5f337e3bf7be450
lokialex <lokih@ru.is>
2019-12-02 18:09:52 +0000

    updated validation file

--- validation.py
+++ validation.py
@@ -1,11 +1,36 @@
 class Validator():
 
-    def validate_string(name):
-        if name.isalpha():
-            
-        return False
-
-    def number_validation(num):
-        if num.isdigit():
+    def __validate_string(self, string):
+        if string.isalpha():
             return True
         return False
+
+    def __validate_int(self, integer):
+        if integer.isdigit():
+            return True
+        return False
+
+    def validate_employee_name(self, name):
+        self.__validate_string(name)
+
+    def validate_employee_ssn(self, ssn):
+        self.__validate_int(ssn)
+
+    def validate_employee_address(self, address):
+
+        try:
+            name, number = address.split()
+            if self.__validate_string(name) and self.__validate_int(number):
+                return True
+
+        except ValueError:
+            return False
+        
+
+def main():
+    validation_instance = Validator()
+    is_address = validation_instance.validate_employee_address("pulsar 1988")
+    print(is_address)
+
+main()
+
