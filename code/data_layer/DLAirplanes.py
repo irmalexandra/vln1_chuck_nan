612c6f98f222004f3cbbbdaae4630cf8ed5f6894
unknown <Emmsje legend>
2019-12-03 17:40:53 +0000

    airplanedl

--- code/data_layer/DLAirplanes.py
+++ code/data_layer/DLAirplanes.py
@@ -3,9 +3,10 @@ from models.Airplane import Airplane
 class DLAirplanes():
     PLANE_TYPE_ID = 0
     PLANE_NAME = 1
-    PLANE_MAKE = 1
-    PLANE_MODEL = 2
-    MAX_SEATS = 3
+    AIRCRAFT_DICT_PLANE_TYPE = 1
+    AIRCRAFT_DICT_MODEL = 2
+    AIRCRAFT_DICT_CAPACITY = 3
+
     def __init__(self):
         self.all_airplanes_list = []
 
@@ -16,20 +17,23 @@ class DLAirplanes():
 
         type_stream_list = [line.strip().split(",") for line in type_stream]
         type_dict = dict()
-        for aircraft_info in type_stream_list[1:]:
+        for aircraft_info in type_stream_list:
             type_dict[aircraft_info[DLAirplanes.PLANE_TYPE_ID]] = aircraft_info[1:]
-        for item in type_dict.items():
-            print(item)
+
         for line in aircraft_stream:
             line_list = line.strip().split(",")
             new_aircraft = Airplane()
+            plane_type = line_list[DLAirplanes.PLANE_TYPE_ID]
+            new_aircraft.set_name(line_list[DLAirplanes.PLANE_NAME]) #Insignia
+            aircraft_info_list = type_dict[plane_type]
+            new_aircraft.set_make(aircraft_info_list[DLAirplanes.AIRCRAFT_DICT_PLANE_TYPE])   #planeType
+            new_aircraft.set_model(aircraft_info_list[DLAirplanes.AIRCRAFT_DICT_MODEL])    #Model
+            new_aircraft.set_max_seats(aircraft_info_list[DLAirplanes.AIRCRAFT_DICT_CAPACITY])    #Capacity
 
-            new_aircraft.set_name(line_list[DLAirplanes.PLANE_NAME])
 
 
 
-
-            self.all_airplanes_list.append(line.strip("\n").split(","))
+            self.all_airplanes_list.append(new_aircraft)
         aircraft_stream.close()
         type_stream.close()
         return self.all_airplanes_list[1:]
--- code/models/Airplane.py
+++ code/models/Airplane.py
@@ -14,7 +14,7 @@ class Airplane():
         self.__max_seats = max_seats
 
     def __str__(self):
-        return "ID: {:<2}\nCountry: {:>2}\nAirport: {:>2}\nFlight time: {:>2}\nDistance: {:>2}\nContact name: {:>2}\nContact number: {:>2}".format(self.__id, self.__country, self.__airport, self.__flight_time, self.__distance, self.__contact_name, self.__contact_num)
+        return "Country: {:>2}\nAirport: {:>2}\nFlight time: {:>2}\nDistance: {:>2}\nContact name: {:>2}\nContact number: {:>2}".format(self.__country, self.__airport, self.__flight_time, self.__distance, self.__contact_name, self.__contact_num)
 
 
     def get_name(self):
@@ -30,25 +30,27 @@ class Airplane():
         return self.__make
 
     def set_make(self, new_make):
-        if models_validation.validate_airplane_make:
-            self.__make = new_make
-        else:
-            pass
+        # if models_validation.validate_airplane_make:
+        #     self.__make = new_make
+        # else:
+        #     pass
+        pass
 
     def get_model(self):
         return self.__model
 
     def set_model(self, new_model):
-        if models_validation.validate_airplane_model:
-            self.__model = new_model
-        else:
-            pass
-
+        # if models_validation.validate_airplane_model:
+        #     self.__model = new_model
+        # else:
+        #     pass
+        pass
     def get_max_seats(self):
         return self.__max_seats
 
     def set_max_seats(self, new_max_seats):
-        if models_validation.validate_airplane_max_seats:
-            self.__max_seats = new_max_seats
-        else:
-            pass
+        # if models_validation.validate_airplane_max_seats:
+        #     self.__max_seats = new_max_seats
+        # else:
+        #     pass
+        pass
