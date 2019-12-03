0e2da4178101db53adecb306287935e8a6e0e899
R1klus <rikhardur19@ru.is>
2019-12-03 21:54:36 +0000

    voyage complete

--- code/models/Voyage.py
+++ code/models/Voyage.py
@@ -25,78 +25,91 @@ class Voyage():
         return self.departing_flight_num
     
     def set_departing_flight_num(self, new):
-        self.departing_flight_num = new
+        if models_validation.validate_flight_number(new):
+            self.departing_flight_num = new
     
     def get_return_flight_num(self):
         return self.return_flight_num
 
     def set_return_flight_num(self, new):
-        self.return_flight_num = new
+        if models_validation.validate_flight_number(new):
+            self.return_flight_num = new
 
     def get_departing_flight_departing_from(self):
         return self.departing_flight_departing_num
 
     def set_departing_flight_departing_from(self, new):
-        self.departing_flight_departing_from = new
+        if models_validation.validate_city(new):
+            self.departing_flight_departing_from = new
 
     def get_departing_flight_departure_date(self):
         return self.departing_flight_departure_date
     
     def set_departing_flight_departure_date(self, new):
-        self.departing_flight_departure_date = new
+        if models_validation.validate_date_time(new):
+            self.departing_flight_departure_date = new
 
     def get_departing_flight_arrival_date(self):
         return self.departing_arrival_date
     
     def set_departing_flight_arrival_date(self, new):
-        self.departing_flight_arrival_date = new
+        if models_validation.validate_date_time(new):
+            self.departing_flight_arrival_date = new
 
     def get_return_flight_departing_from(self):
         return self.return_flight_departing_from
 
     def set_return_flight_departing_from(self, new):
-        self.return_flight_departure_from = new
+        if models_validation.validate_city(new):
+            self.return_flight_departure_from = new
     
     def get_return_flight_departure_date(self):
         return self.return_flight_departure_date
 
     def set_return_flight_departure_date(self, new):
-        self.return_flight_departure_date = new
+        if models_validation.validate_date_time(new):
+            self.return_flight_departure_date = new
 
     def get_return_flight_arrival_date(self):
         return self.return_flight_arrival_date
     
     def set_return_flight_arrival_date(self, new):
-        self.return_flight_arrival_date = new
+        if models_validation.validate_date_time(new):
+            self.return_flight_arrival_date = new
 
     def get_aircraft_id(self):
         return self.aircraft_id
     
     def set_aircraft_id(self, new):
-        self.aircraft_id = new
+        if models_validation.validate_airplane_id(new):
+            self.aircraft_id = new
 
     def get_captain_id(self):
         return self.captain_id
 
     def set_captain_id(self, new):
-        self.captain_id = new
+        if models_validation.validate_employee_id(new):
+            self.captain_id = new
 
     def get_copilot_id(self):
         return self.copilot_id
     
     def set_copilot_id(self, new):
-        self.copilot_id = new
+        if models_validation.validate_employee_id(new):
+            self.copilot_id = new
 
     def get_fsm_id(self):
         return self.fsm_id
     
     def set_fsm_id(self, new):
-        self.fsm_id = new
+        if models_validation.validate_employee_id(new):
+            self.fsm_id = new
 
     def get_fa_ids(self):
         return self.fa_ids
 
     def set_fa_ids(self, new):
-        self.fa_ids = new
+        if models_validation.validate_employee_id(new):
+            self.fa_ids.append(new)
 
 
