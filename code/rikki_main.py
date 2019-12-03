581b259307fb80992a4f3d5dc716d107cb84f961
R1klus <rikhardur19@ru.is>
2019-12-03 18:05:24 +0000

    tada

---
+++ code/rikki_main.py
@@ -0,0 +1,11 @@
+from ui_layer import UIEmployees
+from ui_layer.UIEmployees import UIEmployees
+
+ui_emp = UIEmployees
+new_emp = ui_emp.Employee()
+new_emp.set_ssn("1234567890")
+
+print(new_emp)
+
+
+
