8bde6c0cb0b676a74c37423a5e6174300e564081
unknown <Emmsje legend>
2019-12-03 17:12:24 +0000

    thing

--- code/data_layer/DLAirplanes.py
+++ code/data_layer/DLAirplanes.py
@@ -1,4 +1,4 @@
-
+from models.Airplane import Airplane
 
 class DLAirplanes():
     PLANE_TYPE_ID = 0
@@ -11,14 +11,26 @@ class DLAirplanes():
 
     def pull_all_airplanes(self):
         
-        filestream = open("./repo/Aircraft.csv", "r")
-        for line in filestream:
+        aircraft_stream = open("./repo/Aircraft.csv", "r")
+        type_stream = open("./repo/AircraftType.csv", "r")
+
+        type_stream_list = [line.strip().split(",") for line in type_stream]
+        type_dict = dict()
+        for aircraft_info in type_stream_list[1:]:
+            type_dict[aircraft_info[DLAirplanes.PLANE_TYPE_ID]] = aircraft_info[1:]
+
+        for line in aircraft_stream:
             line_list = line.strip().split(",")
+            new_aircraft = Airplane()
+
+            new_aircraft.set_name(line_list[DLAirplanes.PLANE_NAME])
+
 
 
 
             self.all_airplanes_list.append(line.strip("\n").split(","))
-        filestream.closed
+        aircraft_stream.close()
+        type_stream.close()
         return self.all_airplanes_list[1:]
 
     def push_all_airplanes(self):
