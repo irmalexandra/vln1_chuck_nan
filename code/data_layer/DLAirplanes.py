5a6c0cb2d770eba2d77c9f741d58ebacff49c50f
unknown <Emmsje legend>
2019-12-03 17:43:14 +0000

    awaw

--- code/data_layer/DLAirplanes.py
+++ code/data_layer/DLAirplanes.py
@@ -9,15 +9,9 @@ from models.Pilot import Pilot
 class DLAirplanes():
     PLANE_TYPE_ID = 0
     PLANE_NAME = 1
-<<<<<<< HEAD
     AIRCRAFT_DICT_PLANE_TYPE = 1
     AIRCRAFT_DICT_MODEL = 2
     AIRCRAFT_DICT_CAPACITY = 3
-=======
-    PLANE_MAKE = 1
-    PLANE_MODEL = 2
-    MAX_SEATS = 3
->>>>>>> 9d184cf7a496ffd6d4da8a63d48d6f3101d0237e
 
     def __init__(self):
         self.all_airplanes_list = []
@@ -29,17 +23,9 @@ class DLAirplanes():
 
         type_stream_list = [line.strip().split(",") for line in type_stream]
         type_dict = dict()
-<<<<<<< HEAD
         for aircraft_info in type_stream_list:
             type_dict[aircraft_info[DLAirplanes.PLANE_TYPE_ID]] = aircraft_info[1:]
 
-=======
-        for aircraft_info in type_stream_list[1:]:
-            type_dict[aircraft_info[DLAirplanes.PLANE_TYPE_ID]
-                      ] = aircraft_info[1:]
-        for item in type_dict.items():
-            print(item)
->>>>>>> 9d184cf7a496ffd6d4da8a63d48d6f3101d0237e
         for line in aircraft_stream:
             line_list = line.strip().split(",")
             new_aircraft = Airplane()
@@ -50,14 +36,10 @@ class DLAirplanes():
             new_aircraft.set_model(aircraft_info_list[DLAirplanes.AIRCRAFT_DICT_MODEL])    #Model
             new_aircraft.set_max_seats(aircraft_info_list[DLAirplanes.AIRCRAFT_DICT_CAPACITY])    #Capacity
 
-<<<<<<< HEAD
 
 
 
             self.all_airplanes_list.append(new_aircraft)
-=======
-            self.all_airplanes_list.append(line.strip("\n").split(","))
->>>>>>> 9d184cf7a496ffd6d4da8a63d48d6f3101d0237e
         aircraft_stream.close()
         type_stream.close()
         return self.all_airplanes_list[1:]
