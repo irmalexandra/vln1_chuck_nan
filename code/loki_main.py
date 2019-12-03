37d60a1e9453b68c1a53753865ecebf5a8a7ce07
lokialex <lokih@ru.is>
2019-12-03 17:17:20 +0000

    adding imports

--- code/loki_main.py
+++ code/loki_main.py
@@ -1,9 +1,12 @@
 from models.Employee import Employee
+#from ui_layer.UIMain import UIMain
 
 
 def main():
+    #uimain = UIMain()
+    #uimain.
     employee_test = Employee()
-    employee_test.set_ssn("22325444444")
+    employee_test.set_ssn("2232544444")
     print(employee_test.get_ssn())
 
 
