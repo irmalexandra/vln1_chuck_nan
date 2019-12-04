956ad72f34982d5f457ec64c73c790a75cfc7327
unknown <loki.hopkins@gmail.com>
2019-12-04 22:25:19 +0000

    derp

--- code/loki_main.py
+++ code/loki_main.py
@@ -6,7 +6,8 @@ new_ui_emp.display_employee_search_menu()
 new_emp = new_ui_emp.get_employee_by_ssn("1811931544")
 new_emp.set_ssn("1234567890")
 new_emp.set_email("hakon.stjani@nanair.is")
-print("\n" + new_emp)
+print("")
+print(new_emp)
 
 
 #new_ui_emp.display_all_employees_by_title("Pilot")
