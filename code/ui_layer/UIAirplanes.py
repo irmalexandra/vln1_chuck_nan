6dc4e744b8a30b4da08714290016f9f92c4eb1d9
mg <margret@margud.com>
2019-12-04 16:05:10 +0000

    change

--- code/ui_layer/UIAirplanes.py
+++ code/ui_layer/UIAirplanes.py
@@ -16,29 +16,35 @@ class UIAirplanes():
         self.ll_api = LLAPI()
 
     def display_airplanes_sub_menu(self):
+        ''' Print the airplanes menu '''
         airplane_menu = "1. Create 2. Display all"
         print("-" * self.UI_DIVIDER_INT)
         print("|{}{}{}|".format(airplane_menu, " "*(self.UI_DIVIDER_INT - len(airplane_menu) -
                                                     len(self.RETURN_MENU_STR)-self.DEVIATION_INT), self.RETURN_MENU_STR))
         print("-" * self.UI_DIVIDER_INT)
 
-    def display_airplanes(self):
-        print("ID    Make    Model   TotalSeats  Status  Destination  Flight number  Date available  Time available")
-
     def create_airplane(self):
+        ''' Create an airplane '''
+        # 1
         name = input("ID: ")
         make = input("Make: ")
         model = input("Model: ")
         total_seats = input("Total seats: ")
 
     def display_all_airplanes(self):
+        ''' Print all airplanes '''
+        # 2
         print("-" * self.UI_DIVIDER_INT)
-        print("{:12}{:12}{:12}{:12}".format(
-            "ID:", "Make:", "Model:", "Total seats:"))
+        print("{:11}{:11}{:12}{:17}{:11}{:17}{:17}{:12}".format(
+            "ID:", "Make:", "Model:", "Total seats:", "Status:", "Destination:", "Flight number:", "Date available:"))
         print("-" * self.UI_DIVIDER_INT)
         airplanes_list = self.ll_api.get_all_airplanes_list()
         for airplanes in airplanes_list[1:]:
-            print("{:12}{:12}{:12}{:12}".format(airplanes.get_name(),
-                                                airplanes.get_make(),
-                                                airplanes.get_model(),
-                                                airplanes.get_max_seats()))
+            print("{:11}{:11}{:12}{:17}{:11}{:17}{:17}{:12}".format(airplanes.get_name(),
+                                                                    airplanes.get_make(),
+                                                                    airplanes.get_model(),
+                                                                    airplanes.get_max_seats(),
+                                                                    "Missing status",
+                                                                    "Missing destination",
+                                                                    "Missing flight_number",
+                                                                    "Missing date_available"))
