fb49ebab5e4ead8f45823f73b2fbdd03450d63a0
mg <margret@margud.com>
2019-12-05 13:32:41 +0000

    change

--- code/logic_layer/LLEmployees.py
+++ code/logic_layer/LLEmployees.py
@@ -6,21 +6,29 @@ class LLEmployees:
     def validate_employee(self):
         pass
 
-    def get_employee_by_ssn(self, ssn):
-        ''' pulls a list of employee instances and returns a instance of employee by employee_ID '''
-
-        employee_list = self.__dl_api.populate_all_employees()
-        for employee in employee_list:
-            if employee.get_ssn() == ssn:
-                return employee
-
     def get_all_employees(self):
         ''' pulls and returns a list of employee instances '''
         employee_list = self.__dl_api.populate_all_employees()
         employee_dict = {}
+        index = 1
         for index, employee in enumerate(employee_list):
             employee_dict[index] = employee
         return employee_dict
+
+    def get_ssn_dict(self):
+        employee_dict = self.get_all_employees
+        ssn_dict = {}
+        for index, employee in employee_dict.items():
+            ssn_dict[employee.get_ssn()] = index
+        return ssn_dict
+
+    def get_employee_by_ssn(self, ssn):
+        ''' pulls a list of employee instances and returns a instance of employee by employee_ID '''
+        ssn_dict = self.get_ssn_dict
+        employee_id = ssn_dict[ssn]
+        employee_dict = self.get_all_employees
+        employee = employee_dict[employee_id]
+        return employee
 
     def list_all_employees_by_date(self):
         pass
@@ -40,7 +48,7 @@ class LLEmployees:
     def filter_pilots_by_airplane_type(self, airplane_type):
         pass
 
-    def sort_pilots_bu_airplane_type(self):
+    def sort_pilots_by_airplane_type(self):
         pass
 
     def create_work_scedule(self):
