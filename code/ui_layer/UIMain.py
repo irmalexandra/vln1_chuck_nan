a7aa3d853184a9b2d4a9b820a24901831747a409
lokialex <lokih@ru.is>
2019-12-05 18:51:56 +0000

    fixed exit

--- code/ui_layer/UIMain.py
+++ code/ui_layer/UIMain.py
@@ -46,7 +46,7 @@ class UIMain():
     def display_main_menu(self):
         while True:
             nav_dict = {1: self.__ui_employees.display_employee_sub_menu, 2: self.__ui_voyages.display_voyage_sub_menu,
-                        3: self.__ui_destinations.display_destination_sub_menu, 4: self.__ui_airplanes.display_airplanes_sub_menu, 0: self.__ui_base_functions.exit_program()}
+                        3: self.__ui_destinations.display_destination_sub_menu, 4: self.__ui_airplanes.display_airplanes_sub_menu, 0: self.__ui_base_functions.exit_program}
             self.print_nan_airlines()
             main_menu = "1. Employees 2. Voyages 3. Destinations 4. Airplanes"
             #print("{}{}".format(true_menu, " " * (UIMain.TRUE_LEN - len(true_menu)-1)))
