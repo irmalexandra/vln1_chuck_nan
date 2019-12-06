f7cfb196b60850d64c7570c2c2d7eb4b7fb83816
mg <margret@margud.com>
2019-12-06 17:31:32 +0000

    update employee set functions

--- code/models/Employee.py
+++ code/models/Employee.py
@@ -34,7 +34,6 @@ class Employee():
         return self.__ssn + "," + self.__name + "," + str(self.__address) + "," + str(self.__home_num) + "," + str(self.__mobile_num) + "," + self.__email + "," + self.__title + "," + self.__rank + "\n"
 
     def __str__(self):
-
         return "Name: {:>2} \nSSN: {:>2} \nAddress: {:>2} \nHome number: {:>2} \nMobile number: {:>2} \nEmail: {:>2} \nTitle: {:>2} \nRank: {:>2}".format(self.__name, self.__ssn, self.__address, self.__home_num, self.__mobile_num, self.__email, self.__title, self.__rank)
 
     def get_name(self):
@@ -43,14 +42,14 @@ class Employee():
     def set_name(self, new_name):
         if self.__models_validation.validate_employee_name(new_name):
             self.__name = new_name
-        else:
-            pass
+            return True
+
+        return False
 
     def get_ssn(self):
         return self.__ssn
 
     def set_ssn(self, new_ssn):
-
         if self.__models_validation.validate_employee_ssn(new_ssn):
             self.__ssn = new_ssn
             return True
@@ -64,8 +63,8 @@ class Employee():
         if self.__models_validation.validate_employee_address(new_address):
             self.__address = new_address
             return True
-        else:
-            return False
+
+        return False
 
     def get_home_num(self):
         return self.__home_num
@@ -73,6 +72,9 @@ class Employee():
     def set_home_num(self, new_home_num):
         if self.__models_validation.validate_home_number(new_home_num):
             self.__home_num = new_home_num
+            return True
+
+        return False
 
     def get_mobile_num(self):
         return self.__mobile_num
@@ -80,8 +82,9 @@ class Employee():
     def set_mobile_num(self, new_mobile_num):
         if self.__models_validation.validate_mobile_number(new_mobile_num):
             self.__mobile_num = new_mobile_num
-        else:
-            pass
+            return True
+
+        return False
 
     def get_email(self):
         return self.__email
@@ -89,17 +92,15 @@ class Employee():
     def set_email(self, new_email):
         if self.__models_validation.validate_email(new_email):
             self.__email = new_email
-        else:
-            pass
+            return True
+
+        return False
 
     def get_title(self):
         return self.__title
 
     def set_title(self, new_title):
-        if self.__models_validation.validate_title(new_title):
-            self.__title = new_title
-        else:
-            pass
+        self.__title = new_title
 
     def get_rank(self):
         return self.__rank
@@ -111,10 +112,6 @@ class Employee():
         return self.__licence
 
     def set_licence(self, new_licence):
-        # if self.__models_validation.validate_pilot_airplane_type:
-        #     self.__airplane_type = new_airplane_type
-        # else:
-        #     pass
         self.__licence = new_licence
 
     def get_model_header_format(self, header_flag):
