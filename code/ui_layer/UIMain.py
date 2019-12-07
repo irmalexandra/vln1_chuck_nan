cfa8f72a0d889440a1c1196acd82b6ca64531431
mg <margret@margud.com>
2019-12-07 15:08:52 +0000

    fix

--- code/ui_layer/UIMain.py
+++ code/ui_layer/UIMain.py
@@ -27,8 +27,11 @@ class UIMain():
 
     def display_main_menu(self):
         while True:
-            nav_dict = {1: self.__ui_employees.display_employee_sub_menu, 2: self.__ui_voyages.display_voyage_sub_menu,
-                        3: self.__ui_destinations.display_destination_sub_menu, 4: self.__ui_airplanes.display_airplanes_sub_menu, 0: self.__ui_base_functions.exit_program}
+            nav_dict = {1: self.__ui_employees.display_employee_sub_menu,
+                        2: self.__ui_voyages.display_voyage_sub_menu,
+                        3: self.__ui_destinations.display_destination_sub_menu,
+                        4: self.__ui_airplanes.display_airplanes_sub_menu,
+                        0: self.__ui_base_functions.exit_program}
             main_menu = "1. Employees 2. Voyages 3. Destinations 4. Airplanes"
             return_bool = self.__ui_base_functions.display_menu(
                 main_menu, nav_dict, None, self.RETURN_MENU_STR)
