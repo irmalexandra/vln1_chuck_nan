4df2523dcf55558fcdcd7c0f99146f0773bec2fa
lokialex <lokih@ru.is>
2019-12-08 16:23:10 +0000

    UI destinations updated

--- code/ui_layer/UIDestinations.py
+++ code/ui_layer/UIDestinations.py
@@ -10,8 +10,65 @@ class UIDestinations():
     #All menu functions
 
     def get_destination_sub_menu(self):
-        pass
+        nav_dict = {1: self.create_destination,
+                    2: self.get_all_destinations,
+                    3: self.get_destination_search_menu,
+                    9: self.__ui_base_functions.back,
+                    0: self.__ui_base_functions.home}
+        destination_menu = "1. Create 2. get all 3. Search by"
+        return_value = self.__ui_base_functions.print_menu(
+            destination_menu, nav_dict)
+        return self.__ui_base_functions.check_return_value(return_value)
 
+    def get_destination_search_menu(self):
+        '''Print the search menu of employee sub menu'''
+        nav_dict = {1: self.get_destination_by_airport,
+                    9: self.__ui_base_functions.back,
+                    0: self.__ui_base_functions.home}
+        destination_menu = "1. Airport name"
+        return_value = self.__ui_base_functions.print_menu(
+            destination_menu, nav_dict)
+        return self.__ui_base_functions.check_return_value(return_value)
+
+    def get_selected_destination_menu(self, employee):
+
+        nav_dict = {1: self.change_contact_name,
+                    2: self.change_contact_number,
+                    9: self.__ui_base_functions.back,
+                    0: self.__ui_base_functions.home}
+        destination_menu = "1. Change Name 2. Change Phone Number"
+        return_value = self.__ui_base_functions.print_menu(
+            destination_menu, nav_dict, employee)
+        return self.__ui_base_functions.check_return_value(return_value)
+
+    def get_select_from_destination_list_menu(self, employee_list):
+        nav_dict = {1: self.__ui_base_functions.select_from_model_list,
+                    9: self.__ui_base_functions.back,
+                    0: self.__ui_base_functions.home}
+        destination_menu = "1. Select destination:"
+        return_value = self.__ui_base_functions.print_menu(destination_menu, nav_dict, employee_list)
+        if return_value != None and return_value != 0:
+            return_value = self.get_selected_destination_menu(return_value)
+        return self.__ui_base_functions.check_return_value(return_value)
+
+    #All list functions
+
+    def get_all_destinations(self):
+        '''Print the given dictionary of destinations'''
+        header_flag = "default"
+        destination_list = self.__ll_api.get_all_destination_list()
+        return_value = self.__ui_base_functions.print_model_list(
+            destination_list, self.__modelAPI, header_flag)
+        return_value = self.get_select_from_destination_list_menu(destination_list)
+        return self.__ui_base_functions.check_return_value(return_value)
+
+    # Specific functions
+
+    def get_destination_by_airport(self):
+        pass
+    
+    def create_destination(self):
+        pass
 
     # def display_destination_sub_menu(self):
     #     ''' Print the destination menu '''
@@ -117,10 +174,10 @@ class UIDestinations():
     #         except KeyError:
     #             print("Invalid input! try again")
 
-    # def change_contact(self):
-    #     string = input("Enter new contact name: ")
-    #     return (0, string)
+    def change_contact_name(self):
+        string = input("Enter new contact name: ")
+        return (0, string)
 
-    # def change_contact_number(self):
-    #     number = input("Enter new contact number: ")
-    #     return (1, number)
+    def change_contact_number(self):
+        number = input("Enter new contact number: ")
+        return (1, number)
