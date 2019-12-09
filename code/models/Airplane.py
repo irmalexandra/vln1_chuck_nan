aafdab5b2e35aff9cda203ae167a2d01bf3f5d4e
mg <margret@margud.com>
2019-12-09 14:22:29 +0000

    some fix

--- code/models/Airplane.py
+++ code/models/Airplane.py
@@ -99,7 +99,7 @@ class Airplane():
 
 
     def get_model_header_format(self, header_flag):
-        return "{:10}{:11}{:11}{:11}{:10}{:12}{:17}{:17}{:18}".format("Index: ",
+        return "{:10}{:12}{:10}{:10}{:12}{:18}{:16}{:17}{:18}".format("Index: ",
                                                                     "Insignia:",
                                                                     "Make:",
                                                                     "Model:",
@@ -109,7 +109,7 @@ class Airplane():
                                                                     "Flight number:",
                                                                     "Date available:")
     def get_model_list_info(self, header_flag):
-        returnObject = ("     {:11}{:11}{:11}{:10}{:12}{:20}{:20}{:18}|\n".format(
+        returnObject = ("     {:12}{:10}{:10}{:12}{:18}{:16}{:17}{:18}|\n".format(
                                                                       self.get_insignia(),
                                                                       self.get_make(),
                                                                       self.get_model(),
