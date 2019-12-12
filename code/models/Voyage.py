a28c018545f278f97a465f15ce88fb00da451cdb
lokialex <lokih@ru.is>
2019-12-12 13:32:26 +0000

    created get all ssn in voyage model

--- code/models/Voyage.py
+++ code/models/Voyage.py
@@ -244,9 +244,8 @@ class Voyage():
         return True
         
     def get_all_crew_ssn(self):
-        #self.get_captain_ssn()
-        pass
-
+        return [self.get_captain_ssn(), self.get_copilot_ssn, self.get_fsm_ssn, self.get_fa_ssns]
+        
     def get_voyage_employee_ssn(self, rank):
         return self.__voyage_employee_ssn_dict[rank]()
 
